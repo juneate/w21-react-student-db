34da7c6c90991ba6575c61566ce66f5657a43e1a
Rocco P <roccopan@gmail.com>
2021-06-17 15:13:35 -0400

    Added faux data

--- src/App.js
+++ src/App.js
@@ -2,6 +2,31 @@ import React from 'react'
 import Students from 'pages/Students'
 
 const App = () => {
+
+  const studentsAr = [
+    {
+      id: 234,
+      name: { first: `Tim`, last: `Berners-Lee` },
+      gpa: 3.8,
+      enrolled: [ `WDDM 113`, `WDDM 114` ],
+      photo: `tim-berners-lee.jpg`
+    }, {
+      id: 256,
+      name: { first: `Grace`, last: `Hopper` },
+      gpa: 3.9,
+      enrolled: [ `WDDM 113`, `WDDM 114`, `WDDM 115` ],
+      photo: `grace-hopper.jpg`
+    }, {
+      id: 263,
+      name: { first: `Alan`, last: `Turing` },
+      gpa: 2.3,
+      enrolled: [],
+      photo: `alan-turing.jpg`
+    }
+  ]
+
+  // Generate one StudentRow per object above.
+
   return (
     <Students />
   )
